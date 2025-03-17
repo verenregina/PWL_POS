780be1d3560363e2faae08f8b0158b2550cb62cb
verenregina <reginaveren62@gmail.com>
2025-03-17 22:47:04 +0700

    Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         
-        $user = UserModel::where('level_id', 2)->count();
-        //dd($user);
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Data User</title>
-    </head>
     <body>
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>Jumlah Pengguna</th>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
             </tr>
             <tr>
-                <td>{{$data}}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
         </table>
-    </body>
+    </body>    
 </html>
