38f576e7d561e4b612c26003dac17fea4cdb9dea
verenregina <reginaveren62@gmail.com>
2025-03-31 17:40:34 +0700

    Jobsheet 5 (Praktikum 3 - Implementasi jQuery Datatable di AdminLTE)

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,23 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route untuk Level
-Route::get('/level', [LevelController::class, 'index']);
-
-// Route untuk Kategori
-Route::get('/kategori', [KategoriController::class, 'index']);
-
-// route untuk user
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+use function Ramsey\Uuid\v1;
 
 // route untuk adminlte welcome
 Route::get('/', [WelcomeController::class,'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+});
