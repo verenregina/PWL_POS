d50fdff74516c90f08c81e218b5473bb4f5dbb44
verenregina <reginaveren62@gmail.com>
2025-06-15 23:50:27 +0700

    Jobsheet 8 - Praktikum 3

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yajra\DataTables\Facades\DataTables;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class BarangController extends Controller
 {
@@ -257,5 +258,23 @@ class BarangController extends Controller
 
     return response()->download($temp_file, $filename)->deleteFileAfterSend(true);
 }
+
+public function export_pdf()
+{
+    $barang = Barang::select('kategori_id','barang_kode','barang_nama','harga_beli','harga_jual')
+                ->orderBy('kategori_id')
+                ->orderBy('barang_kode')
+                ->with('kategori')
+                ->get();
+
+    // use Barryvdh\DomPDF\Facade\Pdf;
+    $pdf = Pdf::loadView('barang.export_pdf', ['barang' => $barang]);
+    $pdf->setPaper('a4', 'portrait'); // set ukuran kertas dan orientasi
+    $pdf->setOption("isRemoteEnabled", true); // set true jika ada gambar dari url
+    $pdf->render();
+
+    return $pdf->stream('Data Barang '.date('Y-m-d H:i:s').'.pdf');
+}
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/barang/import', [BarangController::class, 'import']);
         Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']);
         Route::get('/barang/export_excel', [BarangController::class, 'export_excel']);
+        Route::get('/barang/export_pdf', [BarangController::class, 'export_pdf']);
     });
     
 
