6962a3000ad1d063413e440b261abae75fa318db
verenregina <reginaveren62@gmail.com>
2025-06-16 23:50:13 +0700

    Jobsheet 10 - Praktikum 3

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', App\Http\Controllers\API\RegisterController::class)->name('register');
 Route::post('/login', [LoginController::class, 'login']);
 Route::middleware('auth:api')->get('/user', [LoginController::class, 'user']);
+Route::middleware('auth:api')->post('/logout', [LogoutController::class, 'logout']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
